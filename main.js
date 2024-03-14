7d52da2a969abf8f9d42704036f820f13d29d71d
Jason Nguyen <j1.nguyen@yahoo.com>
2024-03-14 15:19:27 -0700

    wip: started project

---
+++ main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+const COLOR_LOOKUP = ["red", "blue", "yellow", "green"]
+
+/*----- state variables -----*/
+let currentPattern;
+let playerPattern;
+let round;
+let highScore;
+
+
+/*----- cached elements  -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
